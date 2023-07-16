eca87dead60fb0f62ce78ab3d00f7bd1c9081550
smooth3x <daniel32000@gmail.com>
2023-07-16 21:33:37 +0300

    Jenkins Testing #2

--- GITRepo-ex3-Daniel-Ventura/Hello.java
+++ GITRepo-ex3-Daniel-Ventura/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Initiate Test File ...");
+        System.out.println("Initiate Test File...");
     }
 }
